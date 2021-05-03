ea3395e28d92814cabbec716e208f1486cdb765b
Simon Rowe <simon@wiremoons.com>
2021-05-03 09:02:20 +0100

    Improved 'build.sh' script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #
 # Generic script to build project with CMake
 
-if [[ -d ./build ]] ; then
+if [ -d ./build ] ; then
   printf "Found old build directory:  removing...\n"
   rm -Rf ./build
   sync
@@ -15,4 +15,14 @@ sleep 1
 cd ./build
 printf "Starting generation of build files with CMake...\n"
 cmake ..
-printf "\nBuild files created.\nIf no errors were reported above then run: cd build ; make\n\n"
+if [ $? ] ; then
+  printf "CMake ran Ok - start build with 'make' ...\n"
+  make
+  if [ $? ] ; then
+    printf "\nExecuting built application:\n\n"
+    ../bin/amt
+  fi
+ printf "\nBuild completed.\nIf no errors were reported - see compiled binary in sub directory:  bin/\n\n"
+else
+  printf "\nBuild files created.\nCMake errors were reported. See above or try to run: cd build ; make\n\n"
+fi
