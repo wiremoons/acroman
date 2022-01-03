f88c5aa51ed64c7996c15adfab4921115c88079a
Simon Rowe <simon@wiremoons.com>
2022-01-03 13:19:38 +0000

    Updates include: - added note to build.sh about using `codesign` on Apple Silicon

--- build.sh
+++ build.sh
@@ -22,7 +22,8 @@ if [ $? ] ; then
     printf "\nExecuting built application:\n\n"
     ../bin/amt
   fi
- printf "\nBuild completed.\nIf no errors were reported - see compiled binary in sub directory:  bin/\n\n"
+ printf "\nBuild completed.\nIf no errors were reported - see compiled binary in sub directory:  bin/\n"
+ printf "If on macOS using Apple Silicon and you copy or rename 'amt' then use:   codesign -s - -f -vvvvvv --timestamp <new-amt-name>\n\n"
 else
   printf "\nBuild files created.\nCMake errors were reported. See above or try to run: cd build ; make\n\n"
 fi
